747c28c41e5b70c505c5d67debd258b17eaf7d78
Gary Li <garyli0802@yahoo.com>
2022-09-25 01:54:32 -0700

    finished week 1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,52 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            var sub = (minuend - subtrahend);
+            return sub;
+           
+
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            var add = (number1 + number2);
+            return add;
+           
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int var = number1;
+            int var2 = number2;
+            return Math.Min(number1, number2);
+            
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            long multi = (factor1 * factor2);
+            return multi;
+            
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+
         }
 
         public string GetHey()
+
         {
-            throw new NotImplementedException();
+            return "HEY!";
+            
         }
     }
 }
