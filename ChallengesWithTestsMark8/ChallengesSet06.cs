bca116c97ce85df68c5bd8ffaff7ad2a9a421c41
Gary Li <jiaweigaryli@gmail.com>
2022-10-25 00:33:10 -0700

    finished challenge 6

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,27 +9,88 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null) return false;
+            var wordArray = words.ToArray();
+            if (ignoreCase)
+            {
+                int i = 0;
+                foreach(var item in wordArray)
+                {
+                    if (item != null)
+                    {
+                        wordArray[i] = item.ToLower();
+                    }
+                    i++;
+                }
+                if (wordArray.Contains(word.ToLower())) return true;
+                else
+                {
+                    return false;
+                }
+                
+            }
+            else if (wordArray.Contains(word)) return true;
+                else return false;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num == 2 || num == 3 || num == 5 ) return true;
+            if (num > 1)
+            {
+                if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0)
+                {
+                    return false;
+                }
+                 return true;  
+            }
+             return false;  
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            var uniqueLetterIndex = -1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str.IndexOf(str[i]) == str.LastIndexOf(str[i]))
+                {
+                    uniqueLetterIndex = i;
+                }
+            }
+            return uniqueLetterIndex;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            var maxCount = 1;
+            var count = 1;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    count++;
+                    if(count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            return maxCount;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            if (elements == null || n <= 0) return Array.Empty<double>();
+            var nthElementList = new List<double>();
+            for (int i = n-1; i <elements.Count; i += n)
+            {
+                nthElementList.Add(elements[i]);
+            }
+            return nthElementList.ToArray();
         }
     }
 }
